0c0eec00880a6822fd4e4421c875fb01c83cb41f
nlsnnn <lagged.06@mail.ru>
2025-03-23 14:28:04 +0300

    Remove addresses fields from Wallet model

--- chiefpay/types/wallet.py
+++ chiefpay/types/wallet.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
-
-from chiefpay.types.invoice import Address
 
 
 class Wallet(BaseModel):
     id: str
     order_id: str = Field(alias="orderId")
-    addresses: Optional[List[Address]] = Field(default=None)
-    uuid: Optional[str] = Field(default=None)
