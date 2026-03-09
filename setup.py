d8a7c2bd28a59aaf70a1ca27fdf96cafca19df15
newTomas <tom.tomas.36478119@gmail.com>
2026-03-09 08:15:13 +0700

    fix - requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
-def requires():
-    with open("requirements.txt", encoding="utf-16-le") as f:
-        lst = f.read().splitlines()
-        if lst and lst[0].startswith('\ufeff'):
-            lst[0] = lst[0][1:]
-        return lst
-
 def readme():
   with open('README.md', 'r') as f:
     return f.read()
@@ -15,7 +8,34 @@ setup(
     name='chiefpay',
     version='2.0.0',
     packages=find_packages(),
-    install_requires=requires(),
+    install_requires=[
+        'aiohappyeyeballs==2.6.1',
+        'aiohttp==3.13.3',
+        'aiosignal==1.4.0',
+        'annotated-types==0.7.0',
+        'attrs==25.3.0',
+        'bidict==0.23.1',
+        'certifi==2025.6.15',
+        'charset-normalizer==3.4.2',
+        'frozenlist==1.7.0',
+        'h11==0.16.0',
+        'idna==3.10',
+        'multidict==6.6.2',
+        'pip_system_certs==5.3',
+        'propcache==0.3.2',
+        'pydantic==2.11.7',
+        'pydantic_core==2.33.2',
+        'python-engineio==4.12.2',
+        'python-socketio==5.14.0',
+        'requests==2.32.4',
+        'simple-websocket==1.1.0',
+        'typing-inspection==0.4.1',
+        'typing_extensions==4.14.0',
+        'urllib3==2.6.3',
+        'websocket-client==1.8.0',
+        'wsproto==1.2.0',
+        'yarl==1.20.1',
+    ],
     author='nelsn',
     author_email='egor.larrr@gmail.com',
     description='ChiefPay Python SDK',
